ec8738e8739a15bf4edc4328fb500844b5b06f08
Jithendranath8 <145124590+Jithendranath8@users.noreply.github.com>
2024-10-31 18:48:34 +0530

    changed server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 const {connectDB} = require('./src/config/db')
 const PORT = process.env.PORT || 3000;
+const dotenv = require("dotenv").config();
 connectDB().then(() => {
   
   app.listen(PORT, () => {
